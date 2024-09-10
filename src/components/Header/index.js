88dcbeab5acd8a20220ad1d4ca6c542851e1bdfe
Deepak-rock <deepakind777@gmail.com>
2024-09-10 18:15:06 +0530

    Test1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,34 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
 // Write your JS code here
+const Header = () => (
+  <header className="header">
+    <div className="logo-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        className="img-logo"
+        alt="wave"
+      />
+      <p className="logo">Wave</p>
+    </div>
+    <ul className="nav-menu">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </header>
+)
+export default Header
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,14 @@
 // Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-section-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      className="not-found-image"
+      alt="not found"
+    />
+    <h1 className="not-found-heading">Not Found</h1>
+  </div>
+)
+export default NotFound
